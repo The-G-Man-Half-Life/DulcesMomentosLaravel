8803605e1f3fa6bc4619167608880acb953baa6e
The-G-Man-Half-Life <mateomoospina@gmail.com>
2024-11-27 07:59:06 -0500

    feat: added rounded attrubute to nac-link own tag

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,5 +1,5 @@
 @props(['active' => false])
 <a
-class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:text-white' }}rounded-md px-3 py-2 text-base font-medium text-white"
+class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:text-white' }}rounded rounded-md px-3 py-2 text-base font-medium text-white"
 {{$attributes}}
 >{{$slot}}</a>
